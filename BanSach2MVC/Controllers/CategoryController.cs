49df3f17560680e245df0156c9142d025394c614
NguyenHungw <hwajio.o@gmail.com>
2024-02-06 10:39:07 +0700

    Add project files.

---
+++ BanSach2MVC/Controllers/CategoryController.cs
@@ -0,0 +1,111 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+using BanSach2MVC.Data;
+using BanSach2MVC.Models;
+
+namespace BanSachMVC_Unica.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The name must not same DisplayOrder");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["Sucess"] = "Category Create Successful";
+                return RedirectToAction("index");
+            }
+            return View(obj);
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryfromDB = _db.Categories.Find(id);
+            /*  var categoryfromDBFirst = _db.Categories.FirstOrDefault(u=>u.ID == id);
+              var categoryfromDBSingle = _db.Categories.SingleOrDefault(u=>u.ID == id);*/
+            if (categoryfromDB == null)
+            {
+                return NotFound();
+            }
+            return View(categoryfromDB);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The name must not same DisplayOrder");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["Sucess"] = "Category Update Successful";
+                return RedirectToAction("index");
+            }
+            return View(obj);
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryfromDB = _db.Categories.Find(id);
+            /*  var categoryfromDBFirst = _db.Categories.FirstOrDefault(u=>u.ID == id);
+              var categoryfromDBSingle = _db.Categories.SingleOrDefault(u=>u.ID == id);*/
+            if (categoryfromDB == null)
+            {
+                return NotFound();
+            }
+            return View(categoryfromDB);
+        }
+        [HttpPost, ActionName("DeletePost")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj.Name == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                TempData["Sucess"] = "Category Delete Successful";
+                return RedirectToAction("index");
+            }
+
+            return View(obj);
+        }
+    }
+}
---
+++ BanSach2MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BanSach2MVC.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BanSach2MVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
